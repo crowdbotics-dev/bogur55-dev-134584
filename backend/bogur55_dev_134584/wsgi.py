b9ccd0d00b01021f2aa72ab5bdfa63c26405b052
Crowdbotics <engineering@crowdbotics.com>
2024-03-19 06:09:33 +0000

    Initial commit

---
+++ backend/bogur55_dev_134584/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for bogur55_dev_134584 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "bogur55_dev_134584.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bogur55-dev-134584",
+  url: "https://bogur55-dev-134584.botics.co",
+  api: "https://bogur55-dev-134584.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
